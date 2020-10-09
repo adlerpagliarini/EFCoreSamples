089197ad9d2e25ab482fe8e429bdd04bbf56cde4
adlerpagliarini <adlerpagliarini@gmail.com>
2020-10-09 19:09:09 -0300

    Fix Using

--- EFCoreSamples/Infrastructure/Interceptors/CommandInterceptor.cs
+++ EFCoreSamples/Infrastructure/Interceptors/CommandInterceptor.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EFCoreSamples.Infrastructure.Interceptors
